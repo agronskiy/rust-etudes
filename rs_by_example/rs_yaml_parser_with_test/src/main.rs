a4f14d8e6524945031ec14a40b14089bb487a27a
Alexey Gronskiy <alex.gronskiy@snyk.io>
2023-12-16 22:41:02 +0100

    chore: introducing bazel

--- rs_by_example/rs_yaml_parser_with_test/src/main.rs
+++ rs_by_example/rs_yaml_parser_with_test/src/main.rs
@@ -8,17 +8,19 @@ use std::string::String;
 use yaml_rust::YamlLoader;
 
 #[derive(Parser)]
-struct Cli {
-    pattern: String,
-    path: PathBuf,
+struct Args {
+    #[clap(short, long)]
+    yaml: PathBuf,
 }
+
 fn read_file(s: &str) -> String {
     let file_contents = fs::read_to_string(s).expect("Should have been able to read the file");
     file_contents
 }
 
 fn main() {
-    let contents = read_file("some_file.yaml");
+    let args = Args::parse();
+    let contents = read_file(args.yaml.to_str().expect("--yaml should be a valid path"));
     let yaml_contents = YamlLoader::load_from_str(&contents).unwrap();
     println!("Contents {:?}", yaml_contents[0]);
 }
